9ab5688bb75715779d94dfd1d5a2b75165e4624f
aradxxx <>
2021-04-15 01:07:47 +0400

    small ui fixes

--- app/src/main/java/im/bnw/android/domain/settings/Settings.kt
+++ app/src/main/java/im/bnw/android/domain/settings/Settings.kt
@@ -14,13 +14,23 @@ data class Settings(
 open class Setting : Parcelable
 
 sealed class ThemeSettings(val value: String) : Setting() {
+    @Parcelize
     object Default : ThemeSettings("")
+
+    @Parcelize
     object Light : ThemeSettings("light")
+
+    @Parcelize
     object Dark : ThemeSettings("dark")
 }
 
 sealed class LanguageSettings(val value: String) : Setting() {
+    @Parcelize
     object Default : LanguageSettings("")
+
+    @Parcelize
     object English : LanguageSettings("en")
+
+    @Parcelize
     object Russian : LanguageSettings("ru")
 }
--- app/src/main/java/im/bnw/android/presentation/profile/Settings.kt
+++ app/src/main/java/im/bnw/android/presentation/profile/Settings.kt
@@ -3,15 +3,26 @@ package im.bnw.android.presentation.profile
 import androidx.annotation.StringRes
 import im.bnw.android.R
 import im.bnw.android.presentation.profile.adapter.SettingsItem
+import kotlinx.parcelize.Parcelize
 
 sealed class ThemeItem(@StringRes nameResId: Int) : SettingsItem(nameResId) {
+    @Parcelize
     object Default : ThemeItem(R.string.system_default)
+
+    @Parcelize
     object Light : ThemeItem(R.string.light_theme)
+
+    @Parcelize
     object Dark : ThemeItem(R.string.dark_theme)
 }
 
 sealed class LanguageItem(@StringRes nameResId: Int) : SettingsItem(nameResId) {
+    @Parcelize
     object Default : LanguageItem(R.string.system_default)
+
+    @Parcelize
     object English : LanguageItem(R.string.english)
+
+    @Parcelize
     object Russian : LanguageItem(R.string.russian)
 }
