dc244cddc37fd5ec3db8b90566e7ad42e2740e0c
aradxxx <aradxxx@gmail.com>
2021-07-20 15:10:07 +0400

    fix externalForward navigation when open another same screen

--- app/src/main/java/im/bnw/android/presentation/core/navigation/BnwModoReducer.kt
+++ app/src/main/java/im/bnw/android/presentation/core/navigation/BnwModoReducer.kt
@@ -8,12 +8,20 @@ import com.github.terrakok.modo.NavigationState
 @Suppress("SpreadOperator")
 class BnwModoReducer(private val origin: NavigationReducer) : NavigationReducer {
     override fun invoke(action: NavigationAction, state: NavigationState): NavigationState =
-        when (action) {
-            is ExternalForward -> NavigationState(
+        when {
+            action is ExternalForward && isSameScreen(action, state) -> {
+                state
+            }
+            action is ExternalForward -> NavigationState(
                 state.chain + listOf(action.screen, *action.screens)
             )
             else -> {
                 origin.invoke(action, state)
             }
         }
+
+    private fun isSameScreen(action: ExternalForward, state: NavigationState): Boolean {
+        val screenId = action.screens.lastOrNull()?.id ?: action.screen.id
+        return state.chain.lastOrNull()?.id == screenId
+    }
 }
