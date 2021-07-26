bf77728d98bccf547933d8c66186ee3027359cef
aradxxx <aradxxx@gmail.com>
2021-07-26 13:00:20 +0400

    fix dagger modules

--- app/src/main/java/im/bnw/android/di/app/AppComponent.kt
+++ app/src/main/java/im/bnw/android/di/app/AppComponent.kt
@@ -8,7 +8,10 @@ import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
 import im.bnw.android.App
 import im.bnw.android.di.ActivityBindingModule
+import im.bnw.android.di.auth.AuthModule
 import im.bnw.android.di.core.ViewModelFactoryModule
+import im.bnw.android.di.message.MessageModule
+import im.bnw.android.di.profile.ProfileModule
 import im.bnw.android.di.settings.SettingsModule
 import im.bnw.android.di.usermanager.UserManagerModule
 import javax.inject.Singleton
@@ -21,9 +24,11 @@ import javax.inject.Singleton
         ViewModelFactoryModule::class,
         NetworkModule::class,
         DataModule::class,
-        AppModule::class,
         UserManagerModule::class,
         SettingsModule::class,
+        MessageModule::class,
+        AuthModule::class,
+        ProfileModule::class,
         NavigationModule::class,
     ]
 )
