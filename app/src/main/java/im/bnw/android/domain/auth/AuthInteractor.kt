ccab580da8d574fe72edc10a023b69990a31a368
fl0werD <fl0werdxd@gmail.com>
2021-08-16 00:03:56 +0400

    auth refactoring (#36)

--- app/src/main/java/im/bnw/android/domain/auth/AuthInteractor.kt
+++ app/src/main/java/im/bnw/android/domain/auth/AuthInteractor.kt
@@ -1,8 +1,9 @@
 package im.bnw.android.domain.auth
 
 import kotlinx.coroutines.flow.Flow
+import im.bnw.android.domain.core.Result
 
 interface AuthInteractor {
-    suspend fun login(userName: String, password: String)
+    suspend fun login(userName: String, password: String): Result<Unit>
     fun subscribeAuth(): Flow<Boolean>
 }
--- app/src/main/java/im/bnw/android/presentation/auth/AuthViewModel.kt
+++ app/src/main/java/im/bnw/android/presentation/auth/AuthViewModel.kt
@@ -3,34 +3,37 @@ package im.bnw.android.presentation.auth
 import com.github.terrakok.modo.Modo
 import im.bnw.android.R
 import im.bnw.android.domain.auth.AuthInteractor
-import im.bnw.android.domain.core.dispatcher.DispatchersProvider
+import im.bnw.android.domain.core.Result
 import im.bnw.android.presentation.core.BaseViewModel
 import im.bnw.android.presentation.core.DialogEvent
 import im.bnw.android.presentation.util.AuthFailedException
-import kotlinx.coroutines.launch
-import java.io.IOException
 import javax.inject.Inject
+import kotlinx.coroutines.launch
 
 class AuthViewModel @Inject constructor(
+    restoredState: AuthState?,
     modo: Modo,
     private val authInteractor: AuthInteractor,
-    private val dispatchersProvider: DispatchersProvider
 ) : BaseViewModel<AuthState>(
-    AuthState(),
+    restoredState ?: AuthState(),
     modo
 ) {
-    override fun handleException(e: Throwable) {
-        when (e) {
-            is AuthFailedException -> postEvent(DialogEvent(R.string.auth_failed))
-            else -> super.handleException(e)
-        }
+    override fun handleException(e: Throwable) = when (e) {
+        is AuthFailedException -> postEvent(DialogEvent(R.string.auth_failed))
+        else -> super.handleException(e)
     }
 
     fun loginChanged(userName: String) {
+        if (state.loading) {
+            return
+        }
         updateState { it.copy(userName = userName) }
     }
 
     fun passwordChanged(password: String) {
+        if (state.loading) {
+            return
+        }
         updateState { it.copy(password = password) }
     }
 
@@ -38,19 +41,17 @@ class AuthViewModel @Inject constructor(
         if (state.loading) {
             return
         }
-        vmScope.launch(dispatchersProvider.default) {
+        vmScope.launch {
             updateState { it.copy(loading = true) }
-            try {
-                authInteractor.login(state.userName, state.password)
-                authSuccess()
-            } catch (t: IOException) {
-                handleException(t)
-                updateState { it.copy(loading = false) }
+            when (val result = authInteractor.login(state.userName, state.password)) {
+                is Result.Success -> {
+                    backPressed()
+                }
+                is Result.Failure -> {
+                    handleException(result.throwable)
+                    updateState { it.copy(loading = false) }
+                }
             }
         }
-    }
-
-    private fun authSuccess() = vmScope.launch {
-        backPressed()
     }
 }
