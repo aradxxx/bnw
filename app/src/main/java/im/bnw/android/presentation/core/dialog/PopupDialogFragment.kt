40cf2bc0c6d74e15d778705c24d2d9aa0a5a4ddf
fl0werD <fl0werdxd@gmail.com>
2021-08-15 21:29:32 +0400

    remove chip

--- app/src/main/java/im/bnw/android/presentation/core/dialog/PopupDialogFragment.kt
+++ app/src/main/java/im/bnw/android/presentation/core/dialog/PopupDialogFragment.kt
@@ -2,7 +2,6 @@ package im.bnw.android.presentation.core.dialog
 
 import android.app.Dialog
 import android.os.Bundle
-import androidx.appcompat.app.AlertDialog
 import androidx.core.os.bundleOf
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.setFragmentResult
